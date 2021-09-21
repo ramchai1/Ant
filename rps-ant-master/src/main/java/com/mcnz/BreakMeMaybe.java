80095f86272a422f6f4065403f8dc59cfd1c6e68
Ramesh Ramakrishna <ram.events1@gmail.com>
2021-09-21 14:00:21 +0530

    Maven project

--- rps-ant-master/src/main/java/com/mcnz/BreakMeMaybe.java
+++
@@ -1,8 +0,0 @@
-package com.mcnz;
-
-public class BreakMeMaybe {
-	
-	// remove the semi-colon in the next line to break the build with a compile error
-	public static final int MAX_COUNT = 0;
-	
-}
